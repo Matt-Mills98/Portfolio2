e6a24d3005d609a558dd43c5c53d47465c6d51bf
Matt <mattmills32013@gmail.com>
2024-01-05 01:08:55 -0500

    Mobile Testing

--- src/App.jsx
+++ src/App.jsx
@@ -90,136 +90,136 @@ export default function App() {
   };
 
   return (
-    <BrowserRouter>
+    <Box sx={{ bgcolor: ' #0f0f0f' }}>
 
-      <Box sx={{bgcolor:' #0f0f0f'}}>
-        {!loaded &&
-          <Box>
+      <BrowserRouter>
+
+        <Box sx={{ bgcolor: ' #0f0f0f' }}>
+          {!loaded &&
             <LoadingAnimation color={accents[accent]} loaded={loaded}></LoadingAnimation>
-          </Box>
-        }
+          }
 
-        <AppBar position="fixed" sx={{ bgcolor: 'black' }}>
-          <Container sx={{ width: '95vw' }}>
-            <Toolbar disableGutters sx={{}}>
+          <AppBar position="fixed" sx={{ bgcolor: 'black' }}>
+            <Container sx={{ width: '95vw' }}>
+              <Toolbar disableGutters sx={{}}>
 
-              <Box sx={{ flexGrow: 1, marginLeft: 'auto', display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-                >
-                  <Box
-                    component="img"
+                <Box sx={{ flexGrow: 1, marginLeft: 'auto', display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                  >
+                    <Box
+                      component="img"
 
-                    sx={{
-                      width: '35px', height: '35px', filter: filter
+                      sx={{
+                        width: '35px', height: '35px', filter: filter
+                      }}
+                      alt="Mmills Logo"
+                      src="mmillslogowhite.png"
+                    />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'left',
                     }}
-                    alt="Mmills Logo"
-                    src="mmillslogowhite.png"
-                  />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: 'block', md: 'none' }, "& .MuiPaper-root": {
-                      backgroundColor: "black"
-                    }
-                  }}
-                >
-                  <MenuItem key={'Home'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./' onClick={() => { setFilter(false); click(0); handleCloseNavMenu(); }}>
-                    <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Home</Typography>
-                  </MenuItem>
-                  <MenuItem key={'About'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./About' onClick={() => { setFilter(false); click(1); handleCloseNavMenu(); }}>
-                    <Typography sx={{ color: '#CCCCCC' }} textAlign="center">About</Typography>
-                  </MenuItem>
-                  <MenuItem key={'Awards'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./Awards' onClick={() => { setFilter(false); click(2); handleCloseNavMenu(); }}>
-                    <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Awards</Typography>
-                  </MenuItem>
-                  <MenuItem key={'Projects'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./Experience' onClick={() => { setFilter(false); click(3); handleCloseNavMenu(); }}>
-                    <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Experience</Typography>
-                  </MenuItem>
-                  <MenuItem key={'Projects'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./Projects' onClick={() => { setFilter(false); click(4); handleCloseNavMenu(); }}>
-                    <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Projects</Typography>
-                  </MenuItem>
-                </Menu>
-              </Box>
-              <Box sx={{ flexGrow: 1, justifyContent: 'left', display: { xs: 'none', md: 'flex' } }}>
-                <IconButton sx={{ mt: 2, color: 'white', display: 'block', }} component={Link} to='./' onClick={() => { setFilter('invert(89%) sepia(74%) saturate(7174%) hue-rotate(280deg) brightness(104%) contrast(123%)'); handleCloseNavMenu(); handleChange(-1); click(0); }}>
-
-                  <Box
-                    component="img"
-
-                    sx={{
-                      width: '35px', height: '35px', filter: filter
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
                     }}
-                    alt="Mmills Logo"
-                    src="mmillslogowhite.png"
-                  />
-                </IconButton>
-                <Tabs textColor='#CCCCCC' sx={{ my: 2, }} TabIndicatorProps={{
-                  sx: {
-                    backgroundColor: accents[accent],
-                  },
-                }} value={value} onChange={handleChange} aria-label="basic tabs example">
-                  <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(1); }} sx={{ color: 'white', }} label="About" component={Link} to='./About' />
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                      display: { xs: 'block', md: 'none' }, "& .MuiPaper-root": {
+                        backgroundColor: "black"
+                      }
+                    }}
+                  >
+                    <MenuItem key={'Home'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./' onClick={() => { setFilter(false); click(0); handleCloseNavMenu(); }}>
+                      <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Home</Typography>
+                    </MenuItem>
+                    <MenuItem key={'About'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./About' onClick={() => { setFilter(false); click(1); handleCloseNavMenu(); }}>
+                      <Typography sx={{ color: '#CCCCCC' }} textAlign="center">About</Typography>
+                    </MenuItem>
+                    <MenuItem key={'Awards'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./Awards' onClick={() => { setFilter(false); click(2); handleCloseNavMenu(); }}>
+                      <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Awards</Typography>
+                    </MenuItem>
+                    <MenuItem key={'Projects'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./Experience' onClick={() => { setFilter(false); click(3); handleCloseNavMenu(); }}>
+                      <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Experience</Typography>
+                    </MenuItem>
+                    <MenuItem key={'Projects'} sx={{ transition: '.3s', ':hover': { bgcolor: '#222222' } }} component={Link} to='./Projects' onClick={() => { setFilter(false); click(4); handleCloseNavMenu(); }}>
+                      <Typography sx={{ color: '#CCCCCC' }} textAlign="center">Projects</Typography>
+                    </MenuItem>
+                  </Menu>
+                </Box>
+                <Box sx={{ flexGrow: 1, justifyContent: 'left', display: { xs: 'none', md: 'flex' } }}>
+                  <IconButton sx={{ mt: 2, color: 'white', display: 'block', }} component={Link} to='./' onClick={() => { setFilter('invert(89%) sepia(74%) saturate(7174%) hue-rotate(280deg) brightness(104%) contrast(123%)'); handleCloseNavMenu(); handleChange(-1); click(0); }}>
 
-                  <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(2); }} sx={{ color: 'white', }} label="Awards" component={Link} to='./Awards' />
-                  <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(3); }} sx={{ color: 'white', }} label="Experience" component={Link} to='./Experience' />
-                  <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(4); }} sx={{ color: 'white', }} label="Projects" component={Link} to='./Projects' />
-                </Tabs>
+                    <Box
+                      component="img"
+
+                      sx={{
+                        width: '35px', height: '35px', filter: filter
+                      }}
+                      alt="Mmills Logo"
+                      src="mmillslogowhite.png"
+                    />
+                  </IconButton>
+                  <Tabs textColor='#CCCCCC' sx={{ my: 2, }} TabIndicatorProps={{
+                    sx: {
+                      backgroundColor: accents[accent],
+                    },
+                  }} value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(1); }} sx={{ color: 'white', }} label="About" component={Link} to='./About' />
+
+                    <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(2); }} sx={{ color: 'white', }} label="Awards" component={Link} to='./Awards' />
+                    <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(3); }} sx={{ color: 'white', }} label="Experience" component={Link} to='./Experience' />
+                    <Tab onClick={() => { setFilter(false); handleCloseNavMenu(); click(4); }} sx={{ color: 'white', }} label="Projects" component={Link} to='./Projects' />
+                  </Tabs>
 
 
 
-              </Box>
+                </Box>
 
-              <IconButton component={Link} to='https://github.com/Matt-Mills98' sx={{ color: accents[accent], transition: '.3s', ":hover": { color: 'white' } }}><GitHubIcon fontSize='large' /></IconButton>
-              <IconButton component={Link} to='https://www.linkedin.com/in/matt-mills-633b00214/' sx={{ color: accents[accent], transition: '.3s', ":hover": { color: 'white' } }}><LinkedInIcon fontSize='large' /></IconButton>
-              <IconButton href={'mailto:mattmills32013@gmail.com'} sx={{ color: accents[accent], transition: '.3s', ":hover": { color: 'white' } }}><EmailIcon fontSize='large' /></IconButton>
-            </Toolbar>
-          </Container>
+                <IconButton component={Link} to='https://github.com/Matt-Mills98' sx={{ color: accents[accent], transition: '.3s', ":hover": { color: 'white' } }}><GitHubIcon fontSize='large' /></IconButton>
+                <IconButton component={Link} to='https://www.linkedin.com/in/matt-mills-633b00214/' sx={{ color: accents[accent], transition: '.3s', ":hover": { color: 'white' } }}><LinkedInIcon fontSize='large' /></IconButton>
+                <IconButton href={'mailto:mattmills32013@gmail.com'} sx={{ color: accents[accent], transition: '.3s', ":hover": { color: 'white' } }}><EmailIcon fontSize='large' /></IconButton>
+              </Toolbar>
+            </Container>
 
-        </AppBar>
-        <Box>
-          <Box sx={{
-            position: 'fixed', top: 0, width: '99.3vw', height: '100vh', zIndex: 0, visibility: { xs: 'collapse', sm: 'visible' }
-          }}>
-            <Animation shuffle={shuffle} accents={accents} accent={accent} width={screenSize.width} />
+          </AppBar>
+          <Box>
+            <Box sx={{
+              position: 'fixed', top: 0, width: '99.3vw', height: '100vh', zIndex: 0, visibility: { xs: 'collapse', sm: 'visible' }
+            }}>
+              <Animation shuffle={shuffle} accents={accents} accent={accent} width={screenSize.width} />
+            </Box>
+            <Outlet />
+            <Routes>
+
+              <Route index element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Home color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Home></Box>}></Route>
+              <Route path="About" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><About color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></About></Box>}></Route>
+              <Route path="Awards" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Awards color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Awards></Box>}></Route>
+              <Route path="Experience" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Experience color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Experience></Box>}></Route>
+              <Route path="Projects" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Projects color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Projects></Box>}></Route>
+
+            </Routes>
+
+
+
           </Box>
-          <Outlet />
-          <Routes>
-
-            <Route index element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Home color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Home></Box>}></Route>
-            <Route path="About" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><About color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></About></Box>}></Route>
-            <Route path="Awards" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Awards color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Awards></Box>}></Route>
-            <Route path="Experience" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Experience color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Experience></Box>}></Route>
-            <Route path="Projects" element={<Box sx={{ margin: 1, userSelect: 'none', '-webkit-touch-callout': 'none' }}><Projects color={accents[accent]} setValue={setValue} setFilter={setFilter} setAccent={click}></Projects></Box>}></Route>
-
-          </Routes>
-
-
-
         </Box>
-      </Box>
 
 
-    </BrowserRouter>
-
+      </BrowserRouter>
+    </Box>
 
   );
 }
