e33e22c2d0d04b3c2618409a76e3407d1d2b2a69
Matt <mattmills32013@gmail.com>
2024-01-05 00:34:38 -0500

    Color Updates

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,7 @@ export default function App() {
                 </Menu>
               </Box>
               <Box sx={{ flexGrow: 1, justifyContent: 'left', display: { xs: 'none', md: 'flex' } }}>
-                <Button sx={{ mt: 2, color: 'white', display: 'block', }} component={Link} to='./' onClick={() => { setFilter('invert(89%) sepia(74%) saturate(7174%) hue-rotate(280deg) brightness(104%) contrast(123%)'); handleCloseNavMenu(); handleChange(-1); click(0); }}>
+                <IconButton sx={{ mt: 2, color: 'white', display: 'block', }} component={Link} to='./' onClick={() => { setFilter('invert(89%) sepia(74%) saturate(7174%) hue-rotate(280deg) brightness(104%) contrast(123%)'); handleCloseNavMenu(); handleChange(-1); click(0); }}>
 
                   <Box
                     component="img"
@@ -177,7 +177,7 @@ export default function App() {
                     alt="Mmills Logo"
                     src="mmillslogowhite.png"
                   />
-                </Button>
+                </IconButton>
                 <Tabs textColor='#CCCCCC' sx={{ my: 2, }} TabIndicatorProps={{
                   sx: {
                     backgroundColor: accents[accent],
